ac3898d6309e432024a3a62c5a32ab884d243076
boucnik.david@gmail.com <boucnik.david@gmail.com>
2014-03-26 10:54:08 +0100

    Start point :) :D

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordCrackerCentralized
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Cracking cracker = new Cracking();
+            //cracker.RunCracking();
+            Dictionary<string,string> dict = new Dictionary<string,string>(0);
+            dict.Add("Ahoj ! ","By bye !");
+            Console.WriteLine(dict);
+            Console.ReadKey();
+        }
+    }
+}
